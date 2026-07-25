2ee460ae8075737c11cb62d28de639d6f704631a
christoskalokairinos <christoskalokairinos@gmail.com>
2026-07-25 13:39:34 +0300

    Upload Cantina JINNI website

---
+++ menu-data.js
@@ -0,0 +1,298 @@
+window.JINNI_MENU = [
+  {
+    "category": "Pasta",
+    "name": "Red Magic",
+    "gr": "Σάλτσα ντομάτας. Επιλογή linguine ή rigatoni.",
+    "en": "Tomato sauce. Choose linguine or rigatoni.",
+    "price": "8,00 €",
+    "visible": true
+  },
+  {
+    "category": "Pasta",
+    "name": "Boss Bolognese",
+    "gr": "Σάλτσα μπολονέζ. Επιλογή linguine ή rigatoni.",
+    "en": "Bolognese sauce. Choose linguine or rigatoni.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Pasta",
+    "name": "Fire Pasta",
+    "gr": "Πικάντικη arrabbiata. Επιλογή linguine ή rigatoni.",
+    "en": "Spicy arrabbiata. Choose linguine or rigatoni.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Pasta",
+    "name": "White Dream",
+    "gr": "Κρεμώδης λευκή σάλτσα. Επιλογή linguine ή rigatoni.",
+    "en": "Creamy white sauce. Choose linguine or rigatoni.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Fried & Loaded",
+    "name": "Jinni Red Crunch",
+    "gr": "Τραγανό κοτόπουλο με κόκκινη σάλτσα.",
+    "en": "Crispy chicken with red sauce.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Fried & Loaded",
+    "name": "Jinni White Crunch",
+    "gr": "Τραγανό κοτόπουλο με λευκή σάλτσα.",
+    "en": "Crispy chicken with white sauce.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Fried & Loaded",
+    "name": "Crunch Box",
+    "gr": "Τραγανό κοτόπουλο με πατάτες.",
+    "en": "Crispy chicken served with fries.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Fried & Loaded",
+    "name": "Dirty Fries",
+    "gr": "Πατάτες με σάλτσα μπολονέζ.",
+    "en": "Fries loaded with Bolognese sauce.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Sandwiches",
+    "name": "Chicken Hero",
+    "gr": "Sandwich με τραγανό κοτόπουλο και επιλογή υλικών.",
+    "en": "Crispy chicken sandwich with your choice of ingredients.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Sandwiches",
+    "name": "Meat Boss",
+    "gr": "Sandwich με μοσχαρίσιο κιμά και επιλογή υλικών.",
+    "en": "Beef sandwich with your choice of ingredients.",
+    "price": "10,00 €",
+    "visible": true
+  },
+  {
+    "category": "Special & Sweet",
+    "name": "Venetian Legend",
+    "gr": "Κομμάτι παραδοσιακό Βενετσιάνικο παστίτσιο Κυθήρων.",
+    "en": "Slice of traditional Kytherian Venetian pastitsio.",
+    "price": "12,00 €",
+    "visible": true
+  },
+  {
+    "category": "Special & Sweet",
+    "name": "Banana Dog",
+    "gr": "Ψωμάκι sandwich με μπανάνα, Nutella και τριμμένο μπισκότο.",
+    "en": "Sandwich bun with banana, Nutella and crushed biscuit.",
+    "price": "7,00 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Extra τυρί / Extra cheese",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Ketchup",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Mayonnaise",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Mustard",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "BBQ",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Cheddar Sauce",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Sweet Chili",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Extras & Sauces",
+    "name": "Sriracha",
+    "gr": "",
+    "en": "",
+    "price": "1,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Coca-Cola Original",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Coca-Cola Zero",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Coca-Cola Zero χωρίς καφεΐνη",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Fanta Μπλε",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Fanta Ανθρακικό",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Fanta Λεμονάδα",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Sprite",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Pink Schweppes",
+    "gr": "",
+    "en": "",
+    "price": "2,50 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Αμίτα 250 ml / Amita 250 ml",
+    "gr": "",
+    "en": "",
+    "price": "2,00 €",
+    "visible": true
+  },
+  {
+    "category": "Refreshments",
+    "name": "Ανθρακούχο νερό 330 ml / Sparkling water 330 ml",
+    "gr": "",
+    "en": "",
+    "price": "3,00 €",
+    "visible": true
+  },
+  {
+    "category": "Beers",
+    "name": "Άλφα κουτάκι / Alfa can",
+    "gr": "",
+    "en": "",
+    "price": "3,00 €",
+    "visible": true
+  },
+  {
+    "category": "Beers",
+    "name": "Μάμος / Mamos",
+    "gr": "",
+    "en": "",
+    "price": "4,00 €",
+    "visible": true
+  },
+  {
+    "category": "Beers",
+    "name": "Fischer",
+    "gr": "",
+    "en": "",
+    "price": "4,00 €",
+    "visible": true
+  },
+  {
+    "category": "Beers",
+    "name": "Corona",
+    "gr": "",
+    "en": "",
+    "price": "5,00 €",
+    "visible": true
+  },
+  {
+    "category": "Beers",
+    "name": "Corona 0%",
+    "gr": "",
+    "en": "",
+    "price": "4,00 €",
+    "visible": true
+  },
+  {
+    "category": "Signature Cocktails",
+    "name": "Aperol Pink",
+    "gr": "Aperol + Pink Schweppes.",
+    "en": "Aperol + Pink Schweppes.",
+    "price": "7,00 €",
+    "visible": true
+  },
+  {
+    "category": "Signature Cocktails",
+    "name": "Jinni Lagoon",
+    "gr": "Vodka + Blue Curaçao + lime + Sprite.",
+    "en": "Vodka + Blue Curaçao + lime + Sprite.",
+    "price": "7,00 €",
+    "visible": true
+  }
+];
